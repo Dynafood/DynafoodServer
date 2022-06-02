fc5f20de615a4823dbe52fee63266f65502a1387
marcel <marcel.taubert@epitech.eu>
2022-06-02 21:15:24 +0200

    Add 'thanks for signup' email

--- src/modules/email.ts
+++ src/modules/email.ts
@@ -4,11 +4,13 @@ mail_1.setApiKey(process.env.SENDGRID_KEY!);
 
 const verify_template_id: string = 'd-557636803ac34c618d0cacbdd1c5dd29';
 const reset_password_template_id: string = 'd-10543ac9938e420cacc6fc86aa6bc3e3';
+const thanks_for_signup_template_id: string = 'd-58f1334bf277429cba6051fec7a77d9f';
+const sender_email: string = 'marcel.taubert@epitech.eu'; // @todo replace
 
 export const sendResetPasswordEmail = async (name: string, email: string) => {
     mail_1.send({
         from: {
-            email: 'marcel.taubert@epitech.eu', // @todo replace
+            email: sender_email,
             name: 'DynaFood'
         },
         to: {
@@ -25,7 +27,7 @@ export const sendResetPasswordEmail = async (name: string, email: string) => {
 export const sendVerifyEmail = async (name: string, email: string) => {
     mail_1.send({
         from: {
-            email: 'marcel.taubert@epitech.eu', // @todo replace
+            email: sender_email,
             name: 'DynaFood'
         },
         to: {
@@ -33,6 +35,23 @@ export const sendVerifyEmail = async (name: string, email: string) => {
             name: name
         },
         templateId: verify_template_id,
+        dynamicTemplateData: {
+            name: name
+        }
+    }).then(() => { console.log(`email was sent to ${email}`); });
+};
+
+export const sendThanksForSignupEmail = async (name: string, email: string) => {
+    mail_1.send({
+        from: {
+            email: sender_email,
+            name: 'DynaFood'
+        },
+        to: {
+            email: email,
+            name: name
+        },
+        templateId: thanks_for_signup_template_id,
         dynamicTemplateData: {
             name: name
         }
