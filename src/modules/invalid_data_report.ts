258498c7af1c52610a05dec3b572c47cdccdea7b
Pol-Antoine Loiseau <pol-antoine.loiseau@epitech.eu>
2022-07-05 13:04:48 +0200

    [Edit] : Add req/res/status

--- src/modules/invalid_data_report.ts
+++ src/modules/invalid_data_report.ts
@@ -11,19 +11,12 @@ On traite de notre coté -> envoie a OpenFoodFact
 */
 
 import nodemailer from 'nodemailer'
+import { Request, Response } from 'express';
 
 let MAIL_ID = "Area2022dev@gmail.com";
 let MAIL_PSSWORD = "qffcocegpaefcsba";
 
-export const mail_invalid_data = () => {
-    invalid_data_mail("pol_antoine.loiseau@yahoo.fr", "Invalid_Data")
-}
-
-export const mail_missing_product = () => {
-    missing_product_mail("pol_antoine.loiseau@yahoo.fr", "Missing_Product")
-}
-
-export const invalid_data_mail = (destinataire : string, message : string) => {
+export const invalid_data_mail = async (req : Request, res : Response) : Promise<void>=> {
     var transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
@@ -36,21 +29,23 @@ export const invalid_data_mail = (destinataire : string, message : string) => {
 
     var mailOptions = {
         from: '"Dynafood" <Area2022dev@gmail.com>',
-        to: destinataire,
+        to: "pol_antoine.loiseau@yahoo.fr",
         subject: 'Invalid data on a product',
-        text: message
+        text: req.body.message
     };
 
     transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
             console.log(error);
+            res.status(500).send({error : "Error" + error})
         } else {
             console.log('Email sent: ' + info.response);
+            res.status(200).send({response : "Email Sent"})
         }
     });
 }
 
-export const missing_product_mail = (destinataire : string, message : string) => {
+export const missing_product_mail = async (req : Request, res : Response) : Promise<void> => {
     var transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
@@ -63,16 +58,18 @@ export const missing_product_mail = (destinataire : string, message : string) =>
 
     var mailOptions = {
         from: '"Dynafood" <Area2022dev@gmail.com>',
-        to: destinataire,
-        subject: 'Missing product on API',
-        text: message
+        to: "pol_antoine.loiseau@yahoo.fr",
+        subject: 'Invalid data on a product',
+        text: req.body.message
     };
 
     transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
             console.log(error);
+            res.status(500).send({error : "Error" + error})
         } else {
             console.log('Email sent: ' + info.response);
+            res.status(200).send({response : "Email Sent"})
         }
     });
 }
