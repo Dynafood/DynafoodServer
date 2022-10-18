4ac500c10dc089b4da9e5a9ef3f9f173465effcc
marcel <marcel.taubert@epitech.eu>
2022-10-18 17:03:44 +0100

    get country code

--- src/modules/user.ts
+++ src/modules/user.ts
@@ -4,6 +4,7 @@ import { Request, Response } from 'express';
 import { database, JWT } from '../../server_config';
 
 import requestIP from 'request-ip';
+import geoip from 'geoip-lite';
 
 type RestrictionObj = {
     alertactivation: boolean,
@@ -39,6 +40,14 @@ export const createUser = async (req: Request, res: Response) => {
         const passcode: string = await bcrypt.hash(req.body.password, 10);
         const ip = requestIP.getClientIp(req);
         console.log("IP: ", ip);
+        const cc: string | undefined = geoip.lookup(req.body.ip)?.country;
+        console.log("CC: ", cc);
+
+        if (cc === undefined) {                                                                                                    
+            res.status(400).send({ Error: 'Unable to create new User.', Details: "IP lookup failed" });                            
+            return                                                                                                                 
+        }   
+
         const created: QueryResultRow = await database.User.createUser(req.body.firstName, req.body.lastName, req.body.userName, req.body.email, req.body.phoneNumber, passcode);
         const userid: string = created.enduserid;
         const token: string = JWT.create(userid);
