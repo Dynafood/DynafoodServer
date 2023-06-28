ae8394d164320b53049af7b66c618aabf0dd7bfe
ImCookiieZ <53428372+ImCookiieZ@users.noreply.github.com>
2023-06-28 10:36:17 +0200

    fix error where tmux script crashes

--- crash_scripts/tmux_report.py
+++ crash_scripts/tmux_report.py
@@ -2,27 +2,30 @@ import subprocess
 
 def check_tmux_process(session_name, process_name):
     # Check if the tmux session exists
-    session_exists = subprocess.run(['tmux', 'has-session', '-t', session_name], capture_output=True)
+    session_exists = subprocess.run(['tmux', 'has-session', '-t', session_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if session_exists.returncode != 0:
         print(f"Session '{session_name}' does not exist.")
         return
 
     # Get the process status from the tmux session
-    process_status = subprocess.run(['tmux', 'list-panes', '-t', session_name, '-F', '#F #{pane_pid} #{pane_current_command}'], capture_output=True, text=True)
+    process_status = subprocess.run(['tmux', 'list-panes', '-t', session_name, '-F', '#F #{pane_pid} #{pane_current_command}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     process_list = process_status.stdout.strip().split('\n')
     for process_info in process_list:
-        pid, command = process_info.split()
-        if command == process_name:
-            # Check if the process is still running
-            process_running = subprocess.run(['ps', '-p', pid], capture_output=True)
-            if process_running.returncode == 0:
-                print(f"Process '{process_name}' is running.")
-                return
-            else:
-                # Retrieve the error output
-                error_output = subprocess.run(['tmux', 'capture-pane', '-pt', session_name], capture_output=True, text=True)
-                print(f"Process '{process_name}' has exited with error output:\n{error_output.stdout}")
-                return
+        process_info_parts = process_info.split()
+        if len(process_info_parts) >= 2:
+            pid = process_info_parts[1]
+            command = ' '.join(process_info_parts[2:])
+            if command == process_name:
+                # Check if the process is still running
+                process_running = subprocess.run(['ps', '-p', pid], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                if process_running.returncode == 0:
+                    print(f"Process '{process_name}' is running.")
+                    return
+                else:
+                    # Retrieve the error output
+                    error_output = subprocess.run(['tmux', 'capture-pane', '-pt', session_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                    print(f"Process '{process_name}' has exited with error output:\n{error_output.stdout}")
+                    return
 
     # Process not found in the session
     print(f"Process '{process_name}' not found in session '{session_name}'.")
