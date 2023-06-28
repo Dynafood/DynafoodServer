4e544f6df243a93e4a5c1a86261acd97176d3f54
ImCookiieZ <53428372+ImCookiieZ@users.noreply.github.com>
2023-06-28 12:51:40 +0200

    no color try

--- crash_scripts/tmux_report.py
+++ crash_scripts/tmux_report.py
@@ -57,28 +57,28 @@ def check_tmux_process(session_name, process_name):
             # Check if the process is still running
             process_running = subprocess.run(['ps', '-p', pid], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             if process_running.returncode == 0 and command == "npm":
-                print(f"\033[32mProcess '{command}' is running.")
+                print(f"Process '{command}' is running.")
             elif process_running.returncode == 0 and command == "bash":
-                print(f"\033[31mProcess '{command}' is running.")
+                print(f"Process '{command}' is running.")
                 error_output = subprocess.run(['tmux', 'capture-pane', '-pt', session_name], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
                 output = "Last output from 'npm':\n"
-                if error_output.stdout != None:
-                    output += f"\nstdout: {error_output.stdout.decode('utf-8')[-1000:]}"
-                if error_output.stderr != None:
-                    output += f"\nstderr:\n{error_output.stderr.decode('utf-8')[-1000:]}"
+                # if error_output.stdout != None:
+                #     output += f"\nstdout: {error_output.stdout.decode('utf-8')[-1000:]}"
+                # if error_output.stderr != None:
+                #     output += f"\nstderr:\n{error_output.stderr.decode('utf-8')[-1000:]}"
                 email_message = f"The server crashed.\n\n{output}\n\\n Executing fallback script."
-                print("\033[0m" + output)
+                print(output)
                 execute_script(fallback_script)
                 send_email(sendgrid_api_key, sender_email, receiver_email, email_subject, email_message)
                 sleeper = 1800
             else:
                 # Retrieve the error output
                 error_output = subprocess.run(['tmux', 'capture-pane', '-pt', session_name], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True)
-                output = f"\033[31mProcess '{command}' has exited with error output:\n\033[0m"
-                if error_output.stdout != None:
-                    output += f"\nstdout: {error_output.stdout.decode('utf-8')[-1000:]}\n"
-                if error_output.stderr != None:
-                   output += f"\nstderr: {error_output.stderr.decode('utf-8')[-1000:]}"
+                output = f"Process '{command}' has exited with error output:\n"
+                # if error_output.stdout != None:
+                #     output += f"\nstdout: {error_output.stdout.decode('utf-8')[-1000:]}\n"
+                # if error_output.stderr != None:
+                #    output += f"\nstderr: {error_output.stderr.decode('utf-8')[-1000:]}"
                 email_message = f"The server crashed.\n\n{output}\n\\n Executing fallback script."
                 print(output)
                 execute_script(fallback_script)
