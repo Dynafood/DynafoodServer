8f5b80a1eaccc3f3e9379c573b2f5ac077a1ff55
ImCookiieZ <53428372+ImCookiieZ@users.noreply.github.com>
2023-06-28 13:17:26 +0200

    crash report on crash or every 24h for conformation

--- crash_scripts/tmux_report.py
+++ crash_scripts/tmux_report.py
@@ -5,7 +5,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from dotenv import load_dotenv
 import os
-from colorama import Fore, Back, Style
+import datetime
 
 load_dotenv()
 
@@ -31,10 +31,11 @@ def send_email(api_key, sender, recipient, subject, message):
             server.login("apikey", api_key)
             server.send_message(msg)
         print("Email sent successfully.")
+        last_sent = datetime.datetime.now()
     except smtplib.SMTPException as e:
         print("Error sending email:", str(e))
 
-def check_tmux_process(session_name, process_name):
+def check_tmux_process(session_name):
     # Check if the tmux session exists
     session_exists = subprocess.run(['tmux', 'has-session', '-t', session_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if session_exists.returncode != 0:
@@ -44,7 +45,6 @@ def check_tmux_process(session_name, process_name):
     # Get the process status from the tmux session
     process_status = subprocess.run(['tmux', 'list-panes', '-t', session_name, '-F', '#F #{pane_pid} #{pane_current_command}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     process_list = process_status.stdout.decode('utf-8').strip().split('\n')
-    process_found = False
 
     sleeper = 1
 
@@ -53,7 +53,6 @@ def check_tmux_process(session_name, process_name):
         if len(process_info_parts) >= 2:
             pid = process_info_parts[1]
             command = ' '.join(process_info_parts[2:])
-            process_found = True
             # Check if the process is still running
             process_running = subprocess.run(['ps', '-p', pid], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             if process_running.returncode == 0 and command == "npm":
@@ -75,24 +74,20 @@ def check_tmux_process(session_name, process_name):
                 # Retrieve the error output
                 error_output = subprocess.run(['tmux', 'capture-pane', '-pt', session_name], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True)
                 output = f"\033[31mProcess '{command}' has exited with error output:\n\033[0m"
-                # if error_output.stdout != None:
-                #     output += f"\nstdout: {error_output.stdout.decode('utf-8')[-1000:]}\n"
-                # if error_output.stderr != None:
-                #    output += f"\nstderr: {error_output.stderr.decode('utf-8')[-1000:]}"
-                email_message = f"The server crashed.\n\n{output}\n\n Executing fallback script."
+                if error_output.stdout != None:
+                    output += f"\nstdout: {error_output.stdout.decode('utf-8')[-1000:]}\n"
+                if error_output.stderr != None:
+                   output += f"\nstderr: {error_output.stderr.decode('utf-8')[-1000:]}"
+                email_message = f"\033[31mThe server crashed.\n\n{output}\n\n Executing fallback script.\033[0m"
                 print(output)
                 execute_script(fallback_script)
                 send_email(sendgrid_api_key, sender_email, receiver_email, email_subject, email_message)
                 sleeper = 1800
             time.sleep(sleeper)
 
-    if not process_found:
-        print(f"Process '{process_name}' not found in session '{session_name}'.")
-
 
 # Example usage
-session_name = 'DevelopmentDynafood'
-process_name = 'npm'
+session_name = 'DynafoodServerDev' #'DevelopmentDynafood'
 # Path to the script to execute if the API endpoint is inaccessible
 fallback_script = "INSERT SCRIPT LATER HERE"
 
@@ -101,6 +96,8 @@ sendgrid_api_key = os.getenv("SENDGRID_KEY")
 sender_email = "info.dynafood@gmail.com"
 receiver_email = "semetiqcookiez@gmail.com" #TODO: INSERT EMAIL TO SEND TO HERE //use the dynafood crators email
 email_subject = "Server Crashed"
-
+last_sent = None
 while True:
-    check_tmux_process(session_name, process_name)
+    check_tmux_process(session_name)
+    if last_sent != None or last_sent < datetime.datetime.now() - datetime.timedelta(hours=24):
+        send_email(sendgrid_api_key, sender_email, receiver_email, "Server Run Confirmation", "Server is currently runnning")
