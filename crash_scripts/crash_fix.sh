83b77740d43309ab36bc2fb040772c7da82ab70f
ImCookiieZ <imCookiieZ@users.noreply.github.com>
2023-09-12 21:55:24 +0200

    kill and restart
    kill npm if running
    start server

--- crash_scripts/crash_fix.sh
+++ crash_scripts/crash_fix.sh
@@ -2,13 +2,27 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 WHITE='\033[0m'
 
-# echo -e "{RED}kill npm process"
-# tmux send-keys -t "DynafoodServerDev:1" C-c
-# sleep 2
 current_command=$(tmux list-panes -t "DynafoodServerDev:1" -F "#{pane_current_command}")
 
 npm_string="npm"
-if [[ "$cuttent_command" == *"$npm_string"* ]]; then
-    echo "match"
-else
-    echo "no match"
+while [ "$current_command" == *"$npm_string"* ] 
+do
+    echo -e "{RED}kill npm process"
+    tmux send-keys -t "DynafoodServerDev:1" C-c
+    sleep 1
+
+    current_command=$(tmux list-panes -t "DynafoodServerDev:1" -F "#{pane_current_command}")
+
+done
+echo -e "{NC}no npm process found"
+echo -e "{NC}restart server..."
+tmux send-keys -t "DynafoodServerDev:1" "npm start" C-m
+sleep 2
+
+current_command=$(tmux list-panes -t "DynafoodServerDev:1" -F "#{pane_current_command}")
+
+if [[ "$current_command" == *"$npm_string"* ]]; then
+    echo -e "{GREEN}server restarted, npm running"
+else 
+    echo -e "{RED}restart not successfull!!!"
+fi
