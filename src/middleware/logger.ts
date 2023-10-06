c0d8599ac6c673aff5d5e68c837a774d1359d932
ImCookiieZ <53428372+ImCookiieZ@users.noreply.github.com>
2023-10-06 13:54:31 +0200

    prettier loggs on request

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -3,7 +3,8 @@ import { Request, Response, NextFunction } from 'express';
 const logger = (req: Request, res: Response, next: NextFunction): void => {
     // console.log ("Hello, I am the logger");
     console.log(`req:\n\t${req.protocol}://${req.get('host')}${req.originalUrl}`);
-    console.log("\tbody: ", JSON.stringify(req.body));
+    console.log("\tbody: ", JSON.stringify(req.body, null, 2));
+    console.log("\theaders: ", JSON.stringify(req.headers, null, 2));
 
     next();
 };
@@ -12,7 +13,7 @@ const outgoingLogger = (req: Request, res: Response, next: NextFunction) => {
     const originalSend = res.send;
     res.send = function (body: any): Response {
       if (typeof body != "string") {
-        console.log("res:\n\t", res.statusCode, JSON.stringify(body), "\n");
+        console.log("res:\n\t", res.statusCode, JSON.stringify(body, null, 2), "\n");
       }
       originalSend.call(this, body);
       return res
