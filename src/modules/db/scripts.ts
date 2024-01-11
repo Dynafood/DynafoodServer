34def6eee7d2065449e220d37e01eca818607e16
Karl Stoerzel <karl-erik.stoerzel@epitech.eu>
2024-01-11 15:47:48 +0100

    check for string type on escape sql statements

--- src/modules/db/scripts.ts
+++ src/modules/db/scripts.ts
@@ -1,7 +1,7 @@
 import { db_adm_conn } from "./index";
 
 export const checkInputBeforeSqlQuery = (arg: string | null): string => {
-    if (!arg) { return ''; }
+    if (!arg || typeof arg != "string") { return ''; }
     arg = arg.replaceAll("'", "''");
 
     return arg;
